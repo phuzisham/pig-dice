8b7463ff053b6775cabeb704d6ec9f5a000c2813
David Hale and Dan Kiss <david@>
2017-08-23 08:58:49 -0700

    add dice roll buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 
-  // logic for user interface
+// logic for user interface
 $(document).ready(function(){
-
+  $('#p1Form').submit(function(event) {
+    event.preventDefault();
+    var diceRoll = Math.floor(Math.random() * (7 - 1) + 1);
+    console.log(diceRoll);
+  });
+  $('#p2Form').submit(function(event) {
+    event.preventDefault();
+    var diceRoll = Math.floor(Math.random() * (7 - 1) + 1);
+    console.log(diceRoll);
+  });
 });
