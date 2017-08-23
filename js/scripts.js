8f1ad3114b20d4a066cba685ce2d2c0f3cc24172
David Hale and Dan Kiss <dan@>
2017-08-23 13:38:20 -0700

    functional ui made with auto refresh

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,18 @@ function Player(name) {
 Player.prototype.score = function(roll) {
   if (roll === 1) {
     this.totalRoll = 0;
-    alert('Next Player Turn');
+    alert('Next Player Turn...');
     $('.p1,.p2').toggle();
   } else {
     this.totalRoll = this.totalRoll + roll;
   }
 };
+
 var player1 = new Player('Player One');
 var player2 = new Player('Player Two');
 var currentScore1 = 0;
 var currentScore2 = 0;
+
 // logic for user interface
 $(document).ready(function(){
   $('#p1Form').submit(function(event) {
@@ -26,13 +28,20 @@ $(document).ready(function(){
     $('#currentRoll1').text(diceRoll1);
     $('#currentTotal1').text(player1.totalRoll);
   });
+
   $('#hold1').click(function() {
     currentScore1 += player1.totalRoll;
-    $("#totalScore1").text(currentScore1);
-    player1.totalRoll = 0;
-    $('#currentRoll1').text(0);
-    $('#currentTotal1').text(0);
-    $('.p1,.p2').toggle();
+    if (currentScore1 >= 10) {
+      $('.playerOneWins').show();
+      $('.p1').hide();
+      $('.playerScores').hide();
+    } else {
+      $("#totalScore1").text(currentScore1);
+      player1.totalRoll = 0;
+      $('#currentRoll1').text(0);
+      $('#currentTotal1').text(0);
+      $('.p1,.p2').toggle();
+    }
   });
 
   $('#p2Form').submit(function(event) {
@@ -42,12 +51,23 @@ $(document).ready(function(){
     $('#currentRoll2').text(diceRoll2);
     $('#currentTotal2').text(player2.totalRoll);
   });
+
   $('#hold2').click(function() {
     currentScore2 += player2.totalRoll;
-    $("#totalScore2").text(currentScore2);
-    player2.totalRoll = 0;
-    $('#currentRoll2').text(0);
-    $('#currentTotal2').text(0);
-    $('.p1,.p2').toggle();
+    if (currentScore2 >= 10) {
+      $('.playerTwoWins').show();
+      $('.p2').hide();
+      $('.playerScores').hide();
+    } else {
+      $("#totalScore2").text(currentScore2);
+      player2.totalRoll = 0;
+      $('#currentRoll2').text(0);
+      $('#currentTotal2').text(0);
+      $('.p1,.p2').toggle();
+    }
+  });
+
+  $('.playAgain').click(function(event) {
+    location.href = location.href;
   });
 });
