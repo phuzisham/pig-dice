e4603e3342d92412b8fcf4b7360286edb2c07026
David Hale and Dan Kiss <dan@>
2017-08-23 14:55:42 -0700

    add script for computer AI

--- js/play-computer.js
+++ js/play-computer.js
@@ -9,15 +9,16 @@ Player.prototype.score = function(roll) {
     this.totalRoll = 0;
     alert('Next Player Turn...');
     $('.p1,.p2').toggle();
+    playerAI();
   } else {
     this.totalRoll = this.totalRoll + roll;
   }
 };
 
 var player1 = new Player('Player One');
-var player2 = new Player('Player Two');
+var computer = new Player('Computer Player');
 var currentScore1 = 0;
-var currentScore2 = 0;
+var computerScore = 0;
 
 // logic for user interface
 $(document).ready(function(){
@@ -43,29 +44,7 @@ $(document).ready(function(){
       $('#currentRoll1').text(0);
       $('#currentTotal1').text(0);
       $('.p1,.p2').toggle();
-    }
-  });
-
-  $('#p2Form').submit(function(event) {
-    event.preventDefault();
-    var diceRoll2 = Math.floor(Math.random() * (7 - 1) + 1);
-    player2.score(diceRoll2);
-    $('#currentRoll2').text(diceRoll2);
-    $('#currentTotal2').text(player2.totalRoll);
-  });
-
-  $('#hold2').click(function() {
-    currentScore2 += player2.totalRoll;
-    if (currentScore2 >= 10) {
-      $('.playerTwoWins').show();
-      $('.p2').hide();
-      $('.playerScores').hide();
-    } else {
-      $("#totalScore2").text(currentScore2);
-      player2.totalRoll = 0;
-      $('#currentRoll2').text(0);
-      $('#currentTotal2').text(0);
-      $('.p1,.p2').toggle();
+      playerAI();
     }
   });
 
@@ -73,3 +52,35 @@ $(document).ready(function(){
     location.href = location.href;
   });
 });
+
+function playerAI() {
+  var diceRoll1 = Math.floor(Math.random() * (7 - 1) + 1);
+  var diceRoll2 = Math.floor(Math.random() * (7 - 1) + 1);
+  if (diceRoll1 === 1) {
+    computer.totalRoll = 0;
+    alert('Next Player Turn...');
+    $('.p1,.p2').toggle();
+    return;
+  } else if (diceRoll2 === 1) {
+    computer.totalRoll = 0;
+    alert('Next Player Turn...');
+    $('.p1,.p2').toggle();
+    return;
+  } else {
+    computer.score(diceRoll1);
+    computer.score(diceRoll2);
+    computerScore += computer.totalRoll;
+    if (computerScore >= 10) {
+      $('.playerTwoWins').show();
+      $('.p2').hide();
+      $('.playerScores').hide();
+      return;
+    } else {
+      $("#totalScore2").text(computerScore);
+      computer.totalRoll = 0;
+      $('#currentRoll2').text(0);
+      $('#currentTotal2').text(0);
+      $('.p1,.p2').toggle();
+    }
+  }
+}
