cd1d1dbba84d14d6c032d770f68cb1f909e8071b
David Hale <david.hale7@gmail.com>
2017-08-26 12:41:33 -0700

    add alerts for computer moves and wait function

--- js/play-computer.js
+++ js/play-computer.js
@@ -8,13 +8,60 @@ function Player(name) {
 Player.prototype.score = function(roll) {
   if (roll === 1) {
     this.totalRoll = 0;
-    alert('Next Player Turn...');
+    alert('Rolled a One! Next Player Turn...');
     $('.p1,.p2').toggle();
     playerAI();
   } else {
     this.totalRoll = this.totalRoll + roll;
   }
 };
+
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
+
+function playerAI() {
+  var diceRoll1 = Math.floor(Math.random() * (6) + 1);
+  var diceRoll2 = Math.floor(Math.random() * (6) + 1);
+  if (diceRoll1 === 1) {
+    computer.totalRoll = 0;
+    alert('Rolled a One! Next Player Turn...');
+    $('.p1,.p2').toggle();
+    return;
+  } else if (diceRoll2 === 1) {
+    computer.totalRoll = 0;
+    alert('Rolled a One! Next Player Turn...');
+    $('.p1,.p2').toggle();
+    return;
+  } else {
+    computer.score(diceRoll1);
+    alert('Computer rolled: ' + diceRoll1);
+    wait(500);
+    computer.score(diceRoll2);
+    alert('Computer rolled: ' + diceRoll2);
+    wait(500);
+    alert('Computer scored: ' + computer.totalRoll);
+    computer.totalScore += computer.totalRoll;
+    if (computer.totalScore >= 50) {
+      $('.computerWins').show();
+      $('.p2').hide();
+      $('.computerPlayerScores').hide();
+      return;
+    } else if (computer.totalScore < player1.totalScore -10) {
+
+    } else {
+      $("#totalComputerScore").text(computer.totalScore);
+      computer.totalRoll = 0;
+      $('#currentRoll2').text(0);
+      $('#currentTotal2').text(0);
+      $('.p1,.p2').toggle();
+    }
+  }
+}
 
 var player1 = new Player('Player One');
 var computer = new Player('Computer Player');
@@ -24,7 +71,7 @@ $(document).ready(function(){
   $("#totalComputerScore").text(0);
   $('#p1Form').submit(function(event) {
     event.preventDefault();
-    var diceRoll1 = Math.floor(Math.random() * (7 - 1) + 1);
+    var diceRoll1 = Math.floor(Math.random() * (6) + 1);
     player1.score(diceRoll1);
     $('#currentRoll1').text(diceRoll1);
     $('#currentTotal1').text(player1.totalRoll);
@@ -50,37 +97,3 @@ $(document).ready(function(){
     location.href = location.href;
   });
 });
-
-function playerAI() {
-  var diceRoll1 = Math.floor(Math.random() * (7 - 1) + 1);
-  var diceRoll2 = Math.floor(Math.random() * (7 - 1) + 1);
-  if (diceRoll1 === 1) {
-    computer.totalRoll = 0;
-    alert('Next Player Turn...');
-    $('.p1,.p2').toggle();
-    return;
-  } else if (diceRoll2 === 1) {
-    computer.totalRoll = 0;
-    alert('Next Player Turn...');
-    $('.p1,.p2').toggle();
-    return;
-  } else {
-    computer.score(diceRoll1);
-    computer.score(diceRoll2);
-    computer.totalScore += computer.totalRoll;
-    if (computer.totalScore >= 50) {
-      $('.computerWins').show();
-      $('.p2').hide();
-      $('.computerPlayerScores').hide();
-      return;
-    } else if (computer.totalScore < player1.totalScore -10) {
-
-    } else {
-      $("#totalComputerScore").text(computer.totalScore);
-      computer.totalRoll = 0;
-      $('#currentRoll2').text(0);
-      $('#currentTotal2').text(0);
-      $('.p1,.p2').toggle();
-    }
-  }
-}
--- js/play-person.js
+++ js/play-person.js
@@ -8,7 +8,7 @@ function Player(name) {
 Player.prototype.score = function(roll) {
   if (roll === 1) {
     this.totalRoll = 0;
-    alert('Next Player Turn...');
+    alert('Rolled a One! Next Player Turn...');
     $('.p1,.p2').toggle();
   } else {
     this.totalRoll = this.totalRoll + roll;
@@ -23,7 +23,7 @@ $(document).ready(function(){
   $("#totalScore2").text(0);
   $('#p1Form').submit(function(event) {
     event.preventDefault();
-    var diceRoll1 = Math.floor(Math.random() * (7 - 1) + 1);
+    var diceRoll1 = Math.floor(Math.random() * (6) + 1);
     player1.score(diceRoll1);
     $('#currentRoll1').text(diceRoll1);
     $('#currentTotal1').text(player1.totalRoll);
@@ -46,7 +46,7 @@ $(document).ready(function(){
 
   $('#p2Form').submit(function(event) {
     event.preventDefault();
-    var diceRoll2 = Math.floor(Math.random() * (7 - 1) + 1);
+    var diceRoll2 = Math.floor(Math.random() * (6) + 1);
     player2.score(diceRoll2);
     $('#currentRoll2').text(diceRoll2);
     $('#currentTotal2').text(player2.totalRoll);
